463cdda18def6aa0ea57f03770fa8433c9ff6a9f
Jp <jyotiprakashpc@outlook.com>
2022-06-29 11:24:35 +0530

    Test file push

---
+++ test2.java
@@ -0,0 +1,15 @@
+class test2
+{
+	public static void main(String args[])
+	{
+		while(true)
+		{
+				try{
+				System.out.println("test2 hiiiiiiii");
+				Thread.sleep(3000);
+				}
+				catch(Exception e)
+				{System.out.println(e);}
+		}		
+	}
+}
