50091076575600be5be9958d1c9ce19e897cfb04
Jp <jyotiprakashpc@outlook.com>
2022-06-29 11:37:55 +0530

    Add all my files

---
+++ test.java
@@ -0,0 +1,15 @@
+class test
+{
+	public static void main(String[] args)
+	{
+		try{
+		while(true){
+		System.out.println("hiiiiiiii");
+		Thread.sleep(3000);
+		}
+		}
+		catch(Exception e){
+			System.out.println(e);
+	}
+	}
+}
